0dd7ad5c5eea8a882e7165e3b5cc2ee0ed55d0de
John Cassidy <jpcassidy@outlook.com>
2022-05-30 08:18:00 -0400

    add GetProductAsync to ProductWorkerService to confirm working client

--- src/ProductWorkerService/Worker.cs
+++ src/ProductWorkerService/Worker.cs
@@ -1,5 +1,8 @@
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductGrpc.Protos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +12,30 @@ using System.Threading.Tasks;
 namespace ProductWorkerService {
     public class Worker : BackgroundService {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _config;
 
-        public Worker(ILogger<Worker> logger) {
-            _logger = logger;
+        public Worker(ILogger<Worker> logger, IConfiguration config) {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _config = config ?? throw new ArgumentNullException(nameof(config));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
             while (!stoppingToken.IsCancellationRequested) {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+
+                using var channel = GrpcChannel.ForAddress(_config.GetValue<string>("WorkerService:ServerUrl"));
+                var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+                _logger.LogInformation("GetProductAsync started..");
+                var response = await client.GetProductAsync(
+                                    new GetProductRequest {
+                                        ProductId = 1
+                                    });
+                _logger.LogInformation("GetProductAsync Response: {product}", response.ToString());
+
+                await Task.Delay(_config.GetValue<int>("WorkerService:TaskInterval"), stoppingToken);
             }
         }
     }
