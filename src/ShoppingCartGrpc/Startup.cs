9e5625c99cff4fa81721c599273b289733f247e1
John Cassidy <jpcassidy@outlook.com>
2022-06-01 08:02:56 -0400

    implement ShoppingCartGrp grp server methods

--- src/ShoppingCartGrpc/Startup.cs
+++ src/ShoppingCartGrpc/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ShoppingCartGrpc.Data;
+using ShoppingCartGrpc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,7 +34,7 @@ namespace ShoppingCartGrpc {
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<ShoppingCartService>();
 
                 endpoints.MapGet("/", async context => {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
